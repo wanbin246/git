fc843de44cf58624d75de528a832a53238e1cfc7
wangbin <wangbin@163.com>
2020-10-22 10:13:52 +0800

    修改git-test  aaaaaaaaaaa

--- src/main/java/com/bjpowernode/test.java
+++ src/main/java/com/bjpowernode/test.java
@@ -7,5 +7,6 @@ package com.bjpowernode;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello  World");
+        System.out.println("aaaaaaaaaaa");
     }
 }
